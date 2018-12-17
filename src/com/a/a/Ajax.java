3bc4c06ec2d9dd4443218299d1f2958c42071698
wangshu <Administrator@hongcheng103>
2018-12-17 16:33:07 +0800

    wangshu commit

--- src/com/a/a/Ajax.java
+++ src/com/a/a/Ajax.java
@@ -1,15 +1,42 @@
 package com.a.a;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 public class Ajax {
 //<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println("dfsafdsafdanhg");
+		
 		System.out.println("HelloWorld");
-		System.out.println("Hello World !!! -+-----+-");
 		
 	}
 //=======
 
 	
 //>>>>>>> branch 'master' of https://github.com/wangshu464113010/test.git
+	
+	
+	
+	public void add() throws FileNotFoundException{
+		PrintWriter pw = new PrintWriter(new File("a.txt"));
+		pw.println(10);
+		pw.flush();
+		System.out.println();
+		PrintStream ps = new PrintStream(new File("a.txt"));
+		ps.println("1234756");
+		ps.close();
+	}
+	
+	public void add123456() throws FileNotFoundException{
+		PrintWriter pw = new PrintWriter(new File("a.txt"));
+		pw.println(10);
+		pw.flush();
+		System.out.println();
+		PrintStream ps = new PrintStream(new File("a.txt"));
+		ps.println("1234756");
+		ps.close();
+	}
 }
