ecc03ea3237a293e7378e75a593cb5cef3436ece
张潜 <Administrator@HC186>
2018-12-17 16:25:57 +0800

    zq

--- src/com/a/b/dsa.java
+++ src/com/a/b/dsa.java
@@ -1,5 +1,10 @@
 package com.a.b;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 public class dsa {
 	private void syso() {
 		System.out.println("dsfdsfgsd");
@@ -11,13 +16,11 @@ public class dsa {
 		System.out.println("ndksjfjsk");
 		System.out.println("ndksjfjsk");
 		System.out.println("谁晚一点谁加班");
-		System.out.println("ndksjfjsk");
-		System.out.println("ndksjfjsk");
-		System.out.println("谁晚一点谁加班");
-		System.out.println("ndksjfjsk");
-		System.out.println("ndksjfjsk");
-		System.out.println("谁晚一点谁加班");
 	}
+//<<<<<<< Upstream, based on branch 'master' of https://github.com/wangshu464113010/test.git
+	
+
+//=======
 	public void dfsadfs() {
 		String a = "12";
 		//=======
@@ -26,6 +29,16 @@ public class dsa {
 		System.out.println("ndksjfjsk");
 		System.out.println("ndksjfjsk");
 		System.out.println("谁晚一点谁加班");
+//>>>>>>> 7ec2554 zq
+	}
+	public void add() throws FileNotFoundException{
+		PrintWriter pw = new PrintWriter(new File("a.txt"));
+		pw.println(10);
+		pw.flush();
+		System.out.println();
+		PrintStream ps = new PrintStream(new File("a.txt"));
+		ps.println("1234756");
+		ps.close();
 	}
 	
 }
